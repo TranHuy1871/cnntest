b0685e9b2c83c85f751e5b70666401c21d9e7eb5
Fithuctap09 <fithuctap09@fpts.com.vn>
2023-01-06 15:16:05 +0700

    123

---
+++ TestCNN/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TestCNN
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_proccess = new System.Windows.Forms.Button();
+            this.ptbox_image = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.rtbox_text = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbox_image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_proccess
+            // 
+            this.btn_proccess.Location = new System.Drawing.Point(348, 374);
+            this.btn_proccess.Name = "btn_proccess";
+            this.btn_proccess.Size = new System.Drawing.Size(75, 23);
+            this.btn_proccess.TabIndex = 0;
+            this.btn_proccess.Text = "Proccess";
+            this.btn_proccess.UseVisualStyleBackColor = true;
+            this.btn_proccess.Click += new System.EventHandler(this.btn_proccess_Click);
+            // 
+            // ptbox_image
+            // 
+            this.ptbox_image.Location = new System.Drawing.Point(39, 82);
+            this.ptbox_image.Name = "ptbox_image";
+            this.ptbox_image.Size = new System.Drawing.Size(309, 262);
+            this.ptbox_image.TabIndex = 1;
+            this.ptbox_image.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // rtbox_text
+            // 
+            this.rtbox_text.Location = new System.Drawing.Point(422, 82);
+            this.rtbox_text.Name = "rtbox_text";
+            this.rtbox_text.Size = new System.Drawing.Size(341, 262);
+            this.rtbox_text.TabIndex = 2;
+            this.rtbox_text.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbox_text);
+            this.Controls.Add(this.ptbox_image);
+            this.Controls.Add(this.btn_proccess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbox_image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btn_proccess;
+        private PictureBox ptbox_image;
+        private OpenFileDialog openFileDialog1;
+        private RichTextBox rtbox_text;
+    }
+}
---
+++ TestCNN/Form1.cs
@@ -0,0 +1,274 @@
+﻿namespace TestCNN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Train()
+        {
+            //convert to pixel array
+            Bitmap img = new Bitmap(ptbox_image.Image);
+            double[,,] pixelvalues = new double[3, img.Width, img.Height];
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    pixelvalues[0, i, j] = pixel.R;
+                    pixelvalues[1, i, j] = pixel.G;
+                    pixelvalues[2, i, j] = pixel.B;
+                }
+            }
+
+            //add filter
+            dynamic output = this.Filter(1, 3, 3);
+
+            //convert layer
+            output = this.ConvolutionLayer(pixelvalues, output);
+
+            //acivation layer using ReLU
+            output = this.ActivationLayer(output);
+
+            //max pooling layer with 2x2 filter and strides 2 
+            output = this.MaxPoolingLayer(output, 2);
+
+            //flattern layer
+            output = this.FlatternLayer(output);
+            double[] weights = this.RandomWeights(output.Length);
+
+            //Fully Connected layer
+            output = this.FullyConnectedLayer(output, weights);
+
+            //=============
+            rtbox_text = output;
+        }
+
+        //=========================================================================================================================
+
+        //tích chập
+        public double[,,] ConvolutionLayer(double[,,] input, double[,,,] filter)
+        {
+            double[,,] output = new double[input.GetLength(0), input.GetLength(1), input.GetLength(2)];
+            try
+            {
+                for (int i = 0; i < filter.GetLength(0); i++)
+                {
+                    for (int j = 0; j < input.GetLength(0); j++)
+                    {
+                        for (int k = 0; k < input.GetLength(1); k++)
+                        {
+                            for (int l = 0; l < input.GetLength(2); l++)
+                            {
+                                output[j, k, l] = input[j, k, l] * filter[i, j, k, l];
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return output;
+        }
+
+        //=========================================================================================================================
+
+        //Filter
+        public double[,,,] Filter(int filter, int nooffilters, int pixelsize)
+        {
+            double[,,,] doubleFilter = new double[filter, nooffilters, pixelsize, pixelsize];
+            Random random = new Random();
+            for (int i = 0; i < filter; i++)
+            {
+                for (int j = 0; j < nooffilters; j++)
+                {
+                    for (int k = 0; k < pixelsize; k++)
+                    {
+                        for (int l = 0; l < pixelsize; l++)
+                        {
+                            doubleFilter[i, j, k, l] = random.NextDouble();
+                        }
+                    }
+                }
+            }
+
+            return doubleFilter;
+        }
+
+        //=========================================================================================================================
+
+        //activationlayer
+        public double[,,] ActivationLayer(double[,,] input)
+        {
+            double[,,] output = new double[input.GetLength(0), input.GetLength(1), input.GetLength(2)];
+            try
+            {
+                for (int j = 0; j < input.GetLength(0); j++)
+                {
+                    for (int k = 0; k < input.GetLength(1); k++)
+                    {
+                        for (int l = 0; l < input.GetLength(2); l++)
+                        {
+                            output[j, k, l] = input[j, k, l] < 0 ? 0 : input[j, k, l];
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return output;
+        }
+
+
+        //=========================================================================================================================
+
+        public double[,,] MaxPoolingLayer(double[,,] input, int filtersize)
+        {
+            double[,,] output = null;
+            try
+            {
+                //Formula for MaxPooling
+                // newwidth = Width/filtersize and newheight = Height/filtersize
+                // width+newwidth + width and height+newheight +heigth
+
+                var newHeight = ((input.GetLength(1) - filtersize) / 2) + 1;
+                var newWidth = ((input.GetLength(2) - filtersize) / 2) + 1;
+                //output= new double[input.GetLength(0), input.GetLength(1), input.GetLength(2)]; for (int j = 0; j < input.GetLength(0); j++)
+                output = new double[input.GetLength(0), newHeight, newWidth];
+                for (int j = 0; j < input.GetLength(0); j++)
+                {
+                    var cuurent_y = 0; var out_y = 0;
+                    for (int k = cuurent_y + filtersize; k < input.GetLength(1); k++)
+                    {
+                        var cuurent_x = 0; var out_x = 0;
+                        var rowValue = input[j, k, 0] * newWidth + input[j, k, 0];
+                        for (int l = cuurent_x + filtersize; l < input.GetLength(2); l++)
+                        {
+                            var columnValue = input[j, k, l] * newHeight + input[j, k, l];
+                            double maxValue = MaxValue(input, j, k, l, filtersize);
+                            output[j, out_y, out_x] = input[j, k, l] > maxValue ? input[j, k, l] : maxValue; // using which is maximum value
+                            cuurent_x = cuurent_x + 2;
+                            out_x = out_x + 1;
+                        }
+                        cuurent_y = cuurent_y + 2;
+                        out_y = out_y + 1;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return output;
+        }
+        public double MaxValue(double[,,] input, int i, int j, int k, int filtersize)
+        {
+            double maxValue = 0;
+            try
+            {
+                for (int a = 0; a < j + filtersize; a++)
+                {
+                    for (int b = 0; b < k + filtersize; b++)
+                    {
+                        maxValue = maxValue < input[i, a, b] ? input[i, a, b] : maxValue;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return maxValue;
+        }
+
+        //=========================================================================================================================
+
+
+        public double[] FlatternLayer(double[,,] input)
+        {
+
+            int rgbChannel = input.GetLength(0);
+            int rowPixel = input.GetLength(1);
+            int columnPixel = input.GetLength(2);
+            int length = rgbChannel * rowPixel * columnPixel;
+            double[] output = new double[length];
+            try
+            {
+                int count = 0;
+                for (int i = 0; i < rgbChannel; i++)
+                {
+                    for (int j = 0; j < rowPixel; j++)
+                    {
+                        for (int k = 0; k < columnPixel; k++)
+                        {
+                            output[count] = input[i, j, k];
+                            count = count + 1;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return output;
+        }
+
+        //=========================================================================================================================
+
+
+
+
+        public double FullyConnectedLayer(double[] input, double[] weights)
+        {
+            double sum = 0;
+            try
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    sum = sum + (input[i] * weights[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            return sum;
+        }
+
+
+        public double[] RandomWeights(int count)
+        {
+            double[] weights = new double[count];
+            Random random = new Random();
+            try
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    weights[i] = random.NextDouble();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return weights;
+        }
+
+        //==========================
+
+        private void btn_proccess_Click(object sender, EventArgs e)
+        {
+            Train();
+        }
+    }
+}
